0b84813c976b68029ba1d659efc3271a07e587cc
SACHIN K V <sachin.kv.1011@gmail.com>
2022-11-24 22:13:31 +0530

    issueing

---
+++ src/app/issue-books/issue-books.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-books',
+  templateUrl: './issue-books.component.html',
+  styleUrls: ['./issue-books.component.css']
+})
+export class IssueBooksComponent {
+
+}
